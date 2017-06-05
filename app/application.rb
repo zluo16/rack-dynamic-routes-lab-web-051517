d1e0737493d5b2387b34152667cb0b4214275e45
zluo16 <zluo@alumni.berklee.edu>
2017-06-05 10:46:56 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,26 @@
+class Application
+
+  @@items = Array.new
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if @@items.map{|i|i.name}.include?(item_name)
+        resp.write "#{@@items.find{|i|i.name == item_name}.price}"
+        resp.status = 200
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+end
